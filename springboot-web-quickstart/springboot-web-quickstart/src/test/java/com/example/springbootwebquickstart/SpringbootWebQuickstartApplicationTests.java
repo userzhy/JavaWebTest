70ce1b4b8ddc1e579bd16dee18ace6b95a534585
userzhy <userzhy@qq.com>
2023-09-08 15:07:17 +0800

    mybatis入门测试

---
+++ springboot-web-quickstart/springboot-web-quickstart/src/test/java/com/example/springbootwebquickstart/SpringbootWebQuickstartApplicationTests.java
@@ -0,0 +1,24 @@
+package com.example.springbootwebquickstart;
+
+import com.example.springbootwebquickstart.mapper.UserMapper;
+import com.example.springbootwebquickstart.pojo.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class SpringbootWebQuickstartApplicationTests {
+
+    @Autowired
+    private UserMapper userMapper;
+    @Test
+    public void testListUser() {
+        List<User> userList = userMapper.list();
+        userList.stream().forEach(user -> {
+            System.out.println(user);
+        });
+    }
+
+}
